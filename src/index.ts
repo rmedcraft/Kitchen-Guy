f64b874fd536b42f3b3d42f77e963d12cad75b35
rmedcraft <rwtmedcraft@gmail.com>
2024-11-13 15:32:04 -0500

    first command

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import * as Discord from "discord.js";
 import * as dotenv from "dotenv";
 
+dotenv.config();
+
 const client = new Discord.Client({
     intents: ["Guilds", "GuildMessages", "GuildMessageReactions", "DirectMessageReactions"]
-})
+});
 
 client.on("ready", () => {
     console.log("Bot is ready :O");
 });
 
 // bot code here!
+client.on("interactionCreate", async (interaction) => {
+    if (interaction.isCommand()) {
+        // check commands  
+        if (interaction.commandName === "github") {
+            interaction.channel.send("Code for this bot can be found here: https://github.com/rmedcraft/Kitchen-Guy\n\nFind the rest of my projects at https://github.com/rmedcraft")
+        }
+    }
+});
 
 client.login(process.env.token);
